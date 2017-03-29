c4692f316fb22a21f7ebec54788369d28c507cb4
Taporpone <taporpone@gmail.com>
2017-03-29 19:10:21 +0200

    Edit

--- 8_3/country-app/src/presentional/home.component.js
+++ 8_3/country-app/src/presentional/home.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
     <div className='welcomePage'>
         <h1>SPA presenting basic info about countries with implemented search functionality. Built with React, ReactRouter and Redux.</h1>
-        <a href='#'><img style={{width:120,height:120}} src='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png' alt='GithubIcon' /></a>
+        <a href='https://github.com/Taporpone/Countries'><img style={{width:120,height:120}} src='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png' alt='GithubIcon' /></a>
     </div >
 );
 
